cd32901125e266f6e82c8dd5f82e7b3b1215a8c1
kym <dudals896@gmail.com>
2024-08-02 15:16:54 +0900

    design: HeaderSearch.tex Search Box

--- src/components/HeaderSearch.tsx
+++ src/components/HeaderSearch.tsx
@@ -2,41 +2,72 @@ import React from 'react';
 import LanguageIcon from '@mui/icons-material/Language';
 import { Button } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ListIcon from '@mui/icons-material/List';
 import './HeaderSearch.css'
 import i18n from "../utils/i18n";
 
 export default function HeaderSearch() {
-    const [isModal, setIsModal] = React.useState<boolean>(false);
+    const [isLanguageModal, setIsLanguageModal] = React.useState<boolean>(false);
     const onChangeLanguage = (lag: string) => {
         i18n.changeLanguage(lag);
-        setIsModal(false);
+        setIsLanguageModal(false);
     }
     return (
-        <div className={'header'}>
-            <div>
-                <p>guestHouse</p>
+        <div className={"header"}>
+            <div className={'header-1layer'}>
+                <div className={"header-left"}>
+                    <p>guestHouse</p>
+                </div>
+                <div>
+                </div>
+                <div className={"header-right"}>
+                    <p>{i18n.t("guestHouse_registration")}</p>
+                    <LanguageIcon onClick={() => setIsLanguageModal(!isLanguageModal)}/>
+                    <div className={"header-userList"}>
+                        <ListIcon/>
+                        <AccountCircleIcon/>
+                    </div>
+                </div>
             </div>
             <div>
-
+                <div className={"header-searchBox"}>
+                    <div>
+                        <p>{i18n.t("travel_destination")}</p>
+                        <p>{i18n.t("travel_destination_search")}</p>
+                    </div>
+                    <div>
+                        <p>{i18n.t("check_in")}</p>
+                        <p>{i18n.t("add_date")}</p>
+                    </div>
+                    <div>
+                        <p>{i18n.t("check_out")}</p>
+                        <p>{i18n.t("add_date")}</p>
+                    </div>
+                    <div>
+                        <p>{i18n.t("traveler")}</p>
+                        <p>{i18n.t("add_guest")}</p>
+                    </div>
+                    <div className={"searchIcon-background"}>
+                        <SearchIcon className={"header-searchIcon"}/>
+                    </div>
+                </div>
             </div>
-            <div>
-                <p>{i18n.t("guestHouse_registration")}</p>
-                <LanguageIcon onClick={() => setIsModal(!isModal)}/>
-                {isModal ? (
+            {isLanguageModal ? (
                     <div className={'languageSelector-modal'}>
                         <div>
                             <div>
                                 <div/>
                                 <p>{i18n.t("select_language")}</p>
-                                <CloseIcon onClick={() => setIsModal(false)}/>
+                                <CloseIcon onClick={() => setIsLanguageModal(false)}/>
                             </div>
                             <Button variant="outlined" color={'inherit'} onClick={() => onChangeLanguage('ko')}>한국어<br/>대한민국</Button>
                             <Button variant="outlined" color={'inherit'} onClick={() => onChangeLanguage('en')}>english<br/>U.S.A</Button>
                             <Button variant="outlined" color={'inherit'} onClick={() => onChangeLanguage('jp')}>日本語<br/>日本</Button>
                         </div>
-                    </div>):
-                    null}
-            </div>
+                    </div>) :
+                null}
         </div>
     );
 }
