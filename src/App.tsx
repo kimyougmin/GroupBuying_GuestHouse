aacd8eeb67829948d0cb93ae458e5ca761182395
kym <dudals896@gmail.com>
2024-08-21 03:12:52 +0900

    feat: add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
-import MainScreen from "./MainScreen/MainScreen";
+import MainScreen from "./Screens/MainScreen/MainScreen";
 import LoginModalContext from "./useContext/LoginModalContext";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HouseDetailScreen from "./Screens/HouseDetailScreen/HouseDetailScreen";
 
 function App() {
   return (
     <div className="App">
         <LoginModalContext>
-            <MainScreen />
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<MainScreen />}/>
+                    <Route path={'/:houseNumber'} element={<HouseDetailScreen/>}/>
+                </Routes>
+            </BrowserRouter>
         </LoginModalContext>
     </div>
   );
