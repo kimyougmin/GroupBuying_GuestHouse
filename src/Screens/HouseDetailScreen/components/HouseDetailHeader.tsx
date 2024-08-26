8c500698612e97d0379256d83b7b9f8024fe0f7e
kym <dudals896@gmail.com>
2024-08-27 00:25:19 +0900

    design: HouseDetailHeader.tsx ui

---
+++ src/Screens/HouseDetailScreen/components/HouseDetailHeader.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import i18n from "../../../utils/i18n";
+import LanguageIcon from "@mui/icons-material/Language";
+import ListIcon from "@mui/icons-material/List";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import {HeaderModalManagerBaseDate} from "../../../useContext/HeaderModalManagerBaseDate";
+import {useCookies} from "react-cookie";
+import "../../../styles/HouseDetailHeader.css"
+
+function HouseDetailHeader() {
+    const {setIsLanguageModal, setIsUserModal} = React.useContext(HeaderModalManagerBaseDate);
+    const [cookies, , ] = useCookies(['userToken']);
+    const [profileImg, setProfileImg] = React.useState<string>('');
+
+    React.useEffect(() => {
+        if (cookies.userToken) {
+            fetch(`${process.env.REACT_APP_USER_PROFILE_IMG}`, {
+                headers: {"Authorization": cookies.userToken.token, 'content-type': 'application/json'},
+                method: "GET"
+            }).then((res) => res.json())
+                .then((res) => setProfileImg(res.profileImage))
+                .catch((e) => alert(e))
+        }
+    }, [cookies.userToken]);
+
+    return (
+        <div className={'houseDetailHeader'}>
+            <p>guestHouse</p>
+            <div></div>
+            <div className={"header-right"}>
+                <p>{i18n.t("guestHouse_registration")}</p>
+                <LanguageIcon onClick={() => setIsLanguageModal(true)}/>
+                {cookies.userToken ?
+                    <div className={"header-userList"} onClick={() => setIsUserModal(true)}>
+                        <ListIcon/>
+                        <img src={profileImg}/>
+                    </div> :
+                    <div className={"header-userList"} onClick={() => setIsUserModal(true)}>
+                        <ListIcon/>
+                        <AccountCircleIcon/>
+                    </div>}
+            </div>
+        </div>
+    );
+}
+
+export default HouseDetailHeader;
