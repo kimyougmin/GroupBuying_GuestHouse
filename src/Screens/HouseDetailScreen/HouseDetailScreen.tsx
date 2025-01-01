6c6e063f1a605708e74f984391c1b9a0253deca8
kimyougmin <dudals896@gmail.com>
2025-01-01 16:29:40 +0900

    design: 설명 텍스트 추가

--- src/Screens/HouseDetailScreen/HouseDetailScreen.tsx
+++ src/Screens/HouseDetailScreen/HouseDetailScreen.tsx
@@ -18,6 +18,8 @@ import DetailScreenFetchModel from "../../TestModel/DetailScreenFetchModel";
 import HouseDetailHeaderMobile from "./components/HouseDetailHeaderMobile";
 import {Swiper, SwiperSlide} from "swiper/react";
 import "swiper/css";
+import {Avatar} from "@mui/material";
+
 function HouseDetailScreen() {
     const location = useLocation();
     const {isLoginModal, setIsLoginModal, isLanguageModal} = React.useContext(HeaderModalManagerBaseDate);
@@ -32,11 +34,6 @@ function HouseDetailScreen() {
         houseExplanation: ""
     });
     const [width, setWidth] = React.useState(window.innerWidth);
-    const [imageCount, setImageCount] = React.useState<number>(0);
-    const [style, setStyle] = React.useState({
-        transform: `translateX(-${imageCount}00%)`,
-        transition: `all 0.4s ease-in-out`,
-    });
 
     const handleResize = () => {
         setWidth(window.innerWidth);
@@ -104,26 +101,26 @@ function HouseDetailScreen() {
                         </div>
                     </div>
                 : null}
-
             {width < 780 ? (
                 <div className={"detailBody-m"}>
                     <div className={'detailImage'}>
-                        <div className={"detailImage-mask"}>
-                            <Swiper  pagination={true} className="mySwiper">
-                                {location.state.houseImages.map((e: { url: string | undefined; }, index: React.Key | null | undefined) => {
-                                    return (<SwiperSlide key={index}>
-                                        <img src={e.url}/>
-                                    </SwiperSlide>)
-                                })}
-                            </Swiper>
+                        <Swiper pagination={true} className="mySwiper">
+                            {location.state.houseImages.map((e: { url: string | undefined; }, index: React.Key | null | undefined) => {
+                                return (<SwiperSlide key={index}>
+                                    <img src={e.url}/>
+                                </SwiperSlide>)
+                            })}
+                        </Swiper>
+                    </div>
+                    <div className={'detailExplanation'}>
+                        <p className={'detailExplanation-title'}>{detailDate.detailTitle}</p>
+                        <div className={'detailExplanation-host'}>
                             <div>
-                                <p>{}</p>
+                                <Avatar alt="Remy Sharp" src={detailDate.hostImage} />
+                                <p>{detailDate.hostName}</p>
                             </div>
                         </div>
-
-                    </div>
-                    <div>
-
+                        <p>{detailDate.houseExplanation}</p>
                     </div>
                 </div>) :
 
