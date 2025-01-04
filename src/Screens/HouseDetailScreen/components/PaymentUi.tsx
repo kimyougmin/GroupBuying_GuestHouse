000235b5551b660f0f7860002b403e6ab6b3ece6
kimyougmin <dudals896@gmail.com>
2025-01-04 14:53:16 +0900

    feat: 켈린더 컴포넌트 번역 추가

--- src/Screens/HouseDetailScreen/components/PaymentUi.tsx
+++ src/Screens/HouseDetailScreen/components/PaymentUi.tsx
@@ -6,6 +6,7 @@ import {Button} from "@mui/material";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import CloseIcon from '@mui/icons-material/Close';
 import {useLocation} from "react-router-dom";
+import i18n from "../../../utils/i18n";
 function PaymentUi() {
     const {isCalenderModal, setIsCalenderModal, checkIn, checkOut, count, setCount} = React.useContext(CalenderContext);
     const [isSelectDay, setIsSelectDay] = React.useState<boolean>(true);
@@ -39,7 +40,7 @@ function PaymentUi() {
         <div className='payment-none'>
             <div className={'payment-header'}>
                 <p>₩{reservationInformation.dayPrice.toLocaleString()} </p>
-                <p>/박</p>
+                <p>/{i18n.t("day")}</p>
             </div>
             <div className={'payment-selector'}>
                 {
@@ -70,7 +71,7 @@ function PaymentUi() {
                                 <div>
                                     <div className='checkIn-btn' id={'checkIn'}>
                                         <div id={'checkIn'}>
-                                            <p id={'checkIn'}>check-in</p>
+                                            <p id={'checkIn'}>{i18n.t("check_id")}</p>
                                             <p id={'checkIn'}>{checkIn[0]}. {checkIn[1]}. {checkIn[2]}.</p>
                                         </div>
                                         <CloseIcon className={'closeIcon'}/>
@@ -78,7 +79,7 @@ function PaymentUi() {
                                     <div className={'checkOut-btn'} id={'checkOut'}
                                          onClick={(e) => onSelectCheckInOutHandler(e)}>
                                         <div id={'checkOut'}>
-                                            <p id={'checkOut'}>check-out</p>
+                                            <p id={'checkOut'}>{i18n.t("check_out")}</p>
                                             <p id={'checkOut'}>{checkOut[0]}. {checkOut[1]}. {checkOut[2]}.</p>
                                         </div>
                                         <CloseIcon className={'closeIcon'}/>
@@ -87,26 +88,26 @@ function PaymentUi() {
                             </div>
                         : <div className={'selector-day'}>
                             <div onClick={onOpenModalHandler}>
-                                <p>check-in</p>
+                                <p>{i18n.t("check_in")}</p>
                                 <p>{checkIn[0]}. {checkIn[1]}. {checkIn[2]}.</p>
                             </div>
                             <div onClick={onOpenModalHandler}>
-                                <p>check-out</p>
+                                <p>{i18n.t("check_out")}</p>
                                 <p>{checkOut[0]}. {checkOut[1]}. {checkOut[2]}.</p>
                             </div>
                         </div>
                 }
                 <div className={'selector-person'}>
                     <div>
-                        <p>인원</p>
-                        <p>게스트 1인 </p>
+                        <p>{i18n.t("personnel")}</p>
+                        <p>{i18n.t("guest")} 1{i18n.t("person")} </p>
                     </div>
                     <ArrowBackIosIcon className={'person-icon'}/>
                 </div>
             </div>
             <div className={'payment-footer'}>
                 <Button variant="contained" disableElevation>
-                    예약하기
+                    {i18n.t("reservation_application")}
                 </Button>
                 <span>
                     <p>₩{reservationInformation.dayPrice.toLocaleString()} X {count.toLocaleString()}박</p>
@@ -116,12 +117,12 @@ function PaymentUi() {
                     reservationInformation.cleaningFee === 0 ?
                         null :
                         <span>
-                            <p>청소비</p>
+                            <p>{i18n.t("cleaning_fee")}</p>
                             <p>₩{reservationInformation.cleaningFee.toLocaleString()}</p>
                         </span>
                 }
                 <span>
-                    <p>총 합계</p>
+                    <p>{i18n.t("grand_total")}</p>
                     <p>₩{(reservationInformation.dayPrice * count + reservationInformation.cleaningFee).toLocaleString()}</p>
                 </span>
             </div>
@@ -131,7 +132,7 @@ function PaymentUi() {
                     <div>
                         <button onClick={() => {
                             setIsCalenderModal(false)
-                        }}>close
+                        }}>{i18n.t("close")}
                         </button>
                     </div>
                 </div> : null}
