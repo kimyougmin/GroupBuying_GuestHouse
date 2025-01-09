b43cb6fb4239417e22d60b44c4c383badcc85f9a
kimyougmin <dudals896@gmail.com>
2025-01-09 14:31:54 +0900

    design: 하우스 등록 페이지 ui 업데이트

--- src/Screens/HouseRegistrationScreen/HouseRegistrationScreen.tsx
+++ src/Screens/HouseRegistrationScreen/HouseRegistrationScreen.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import logo from "../../gg_logo.png"
+import i18n from "../../utils/i18n";
+import {Button} from "@mui/material";
+import PhotoModal from "../../components/PhotoModal";
+import { PhotoCamera } from "@mui/icons-material";
+import "../../styles/HouseRegistrationScreen.css"
+import LanguageIcon from "@mui/icons-material/Language";
 
 function HouseRegistrationScreen() {
+    const [isPhotoModal, setIsPhotoModal] = React.useState<boolean>(false);
+
     return (
-        <div></div>
+        <div className={"houseRegistration"}>
+            <div className={"houseRegistration-header"}>
+                <img src={logo}/>
+                <LanguageIcon />
+            </div>
+            <div className={"houseRegistration-body"}>
+                <h1>{i18n.t("add_guestHouse_photo")}</h1>
+                <p>{i18n.t("add_guestHouse_photo_detail")}</p>
+                <div className={"photoArea"}>
+                    <PhotoCamera/>
+                    <div/>
+                    <Button onClick={() => {setIsPhotoModal(!isPhotoModal)}}>{i18n.t("add_photo")}</Button>
+                </div>
+            </div>
+            {isPhotoModal ? <PhotoModal/> : null}
+        </div>
     );
 }
 
---
+++ src/components/PhotoModal.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PhotoModal() {
+    return (
+        <div className={"photoModal-background"}>
+            <div className={"photoModal"}>
+
+            </div>
+        </div>
+    );
+}
+
+export default PhotoModal;
