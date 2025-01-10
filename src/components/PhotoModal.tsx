42b0a52257fa3680043ec3f2be19904d68ee299f
kimyougmin <dudals896@gmail.com>
2025-01-10 10:12:21 +0900

    design: 포토 모달 UI

--- src/components/PhotoModal.tsx
+++ src/components/PhotoModal.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import "../styles/PhotoModal.css"
+import CloseIcon from "@mui/icons-material/Close";
+import {Add, PhotoLibrary} from "@mui/icons-material";
+import {Button} from "@mui/material";
+import i18n from "../utils/i18n";
 
 function PhotoModal() {
     return (
         <div className={"photoModal-background"}>
             <div className={"photoModal"}>
-
+                <div className={"photoModal-header"}>
+                    <CloseIcon />
+                    <div>
+                        <h1>{i18n.t("upload_photo")}</h1>
+                        <p>{i18n.t("no_files_selected")}</p>
+                    </div>
+                    <Add />
+                </div>
+                <div className={"photoModal-body"}>
+                    <PhotoLibrary/>
+                    <h1 className={"dragAndDrop"}>{i18n.t("drag_and_drop_photos")}</h1>
+                    <p>{i18n.t("or")}</p>
+                    <p className={"directly"}>{i18n.t("directly_select")}</p>
+                    <Button>{i18n.t("find")}</Button>
+                </div>
+                <div className={"photoModal-footer"}>
+                    <Button>{i18n.t("close")}</Button>
+                    <Button>{i18n.t("upload")}</Button>
+                </div>
             </div>
         </div>
     );
