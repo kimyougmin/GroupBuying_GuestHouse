0abef3a08d62fa54fe68594016b80d01970d260d
kimyougmin <dudals896@gmail.com>
2025-01-14 15:50:45 +0900

    feat: 압축 유틸리티 추가

---
+++ src/utils/CompressImage.ts
@@ -0,0 +1,21 @@
+import imageCompression from "browser-image-compression";
+
+
+export default async function CompressImage  (file: File){
+    const options = {
+        maxSizeMB: 1,
+        useWebWorker: true,
+        preserveExif: true,
+        initialQuality: 0.8,
+    };
+
+    try {
+        const compressedFile = await imageCompression(file, options);
+
+        console.log("Original file size:", file.size / 1024 / 1024, "MB");
+        console.log("Compressed file size:", compressedFile.size / 1024 / 1024, "MB");
+        return compressedFile;
+    } catch (error) {
+        console.error("Error during image compression", error)
+    }
+}
